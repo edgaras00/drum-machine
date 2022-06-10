98e2ae990749ea12a73679df2ab1bedaff241ca2
Edgar <edgaras993@gmail.com>
2022-06-09 20:51:42 -0400

    Updates

---
+++ precache-manifest.3b001ae046e954e039dba078e46ed2b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3135bfe5dc7c0696cc3d0ed03e722847",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "55620c5434daa6b40106",
+    "url": "/drum-machine/static/css/main.05b3d284.chunk.css"
+  },
+  {
+    "revision": "018e42a80f33c42cfaee",
+    "url": "/drum-machine/static/js/2.0ef35da5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.0ef35da5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55620c5434daa6b40106",
+    "url": "/drum-machine/static/js/main.7d7a0be3.chunk.js"
+  },
+  {
+    "revision": "17c11b66b42c1f1cb1d4",
+    "url": "/drum-machine/static/js/runtime-main.eb71868b.js"
+  }
+]);
